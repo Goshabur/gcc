847561b04a2eb841bcae26bbc7c29e969bfd4c6b
Thomas Schwinge <tschwinge@baylibre.com>
2024-07-04 10:04:54 +0200

    WIP Build system support for '--enable-offload-targets=host_process'

---
+++ libgomp/plugin/plugin-host_process.c
@@ -0,0 +1,10 @@
+/* Host process plugin */
+
+#include "config.h"
+//TODO #include "symcat.h"
+#include "libgomp-plugin.h"
+#include "oacc-plugin.h"
+//TODO #include "gomp-constants.h"
+//TODO #include "oacc-int.h"
+
+//TODO
