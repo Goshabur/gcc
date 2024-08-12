e4955799426c69caed71d9867ba22bf800eba70c
Georgii Burunsuzian <goshabur2003@gmail.com>
2024-08-12 14:02:06 +0300

    remove duplicate lines

--- libgomp/libgomp-plugin.h
+++ libgomp/libgomp-plugin.h
@@ -46,7 +46,6 @@ extern "C" {
 /* Type of offload target device.  Keep in sync with include/gomp-constants.h.  */
 enum offload_target_type
 {
-  OFFLOAD_TARGET_TYPE_HOST_PROCESS = 0,
   OFFLOAD_TARGET_TYPE_HOST = 2,
   OFFLOAD_TARGET_TYPE_HOST_PROCESS = 3,
   OFFLOAD_TARGET_TYPE_NVIDIA_PTX = 5,
